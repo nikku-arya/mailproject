e81ccc300a1c0b3b58a32fcc59d492e4938a41ff
nikku-arya <nkumar4@ma.iitr.ac.in>
2022-04-19 17:12:41 +0530

    add to git

---
+++ src/app.module.ts
@@ -0,0 +1,13 @@
+import { Module } from '@nestjs/common';
+import { AppController } from './app.controller';
+import { AppService } from './app.service';
+import { MailModule } from './mail/mail.module';
+import { AuthModule } from './auth/auth.module';
+import { ConfigModule } from '@nestjs/config';
+
+@Module({
+  imports: [MailModule, AuthModule],
+  controllers: [AppController],
+  providers: [AppService],
+})
+export class AppModule {}
